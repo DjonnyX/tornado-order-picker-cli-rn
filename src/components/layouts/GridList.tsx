b65afa504a6bd1fb2d5d228b170ceefabf10b38e
djonnyx <djonnyx@gmail.com>
2021-03-13 14:08:51 +0300

    feat: deleted animations of the GridLayout

--- src/components/layouts/GridList.tsx
+++ src/components/layouts/GridList.tsx
@@ -1,52 +1,28 @@
 import React, { useState, useCallback } from "react";
-import { View, LayoutChangeEvent, StyleProp, ViewStyle, Animated, Easing } from "react-native";
+import { View, LayoutChangeEvent, StyleProp, ViewStyle } from "react-native";
 
 interface IGridListProps<T = any> {
     children?: never[];
     style?: StyleProp<ViewStyle>;
     padding?: number;
-    disbleStartAnimation?: boolean;
     data: Array<T>;
     itemDimension: number;
     spacing?: number;
-    animationSkipFrames?: number;
     renderItem: (data: { item: T, index: number }) => JSX.Element;
     keyExtractor: (item: T, index: number) => number;
 }
 
 export const GridList = React.memo(({ data, renderItem, style, keyExtractor, spacing = 0, padding = 0,
-    animationSkipFrames = 0, disbleStartAnimation = false, itemDimension }: IGridListProps) => {
-    const [isInit, _setIsInit] = useState(false);
-    const [cellWidth, _setCellWidth] = useState(new Animated.Value(1));
+    itemDimension }: IGridListProps) => {
+    const [cellWidth, _setCellWidth] = useState(0);
     const [gap, _setGap] = useState(spacing * 0.5);
-
-    let cellAnimation: Animated.CompositeAnimation;
-
-    if (!disbleStartAnimation) {
-        cellAnimation = Animated.timing(cellWidth, {
-            useNativeDriver: false,
-            toValue: 0,
-            duration: 250,
-            easing: Easing.cubic,
-        });
-    }
 
     const changeLayoutHandler = useCallback((event: LayoutChangeEvent) => {
         const { x, y, width, height } = event.nativeEvent.layout;
         const numColumns = Math.floor(width / itemDimension);
         const gap = spacing * 0.5;
         const actualItemWidth = Math.floor(width - padding * 2 - numColumns * gap * 2) / numColumns
-        if (cellAnimation) {
-            cellAnimation.stop();
-        }
-        cellAnimation = Animated.timing(cellWidth, {
-            useNativeDriver: false,
-            toValue: actualItemWidth,
-            duration: !disbleStartAnimation || (disbleStartAnimation && isInit) ? 200 : 0,
-            easing: Easing.cubic,
-        });
-        cellAnimation.start();
-        _setIsInit(true);
+        _setCellWidth(actualItemWidth);
     }, []);
 
     return (
@@ -57,12 +33,12 @@ export const GridList = React.memo(({ data, renderItem, style, keyExtractor, spa
             <View style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
                 {
                     data.map((item, index) =>
-                        <Animated.View key={(keyExtractor(item, index))}
+                        <View key={(keyExtractor(item, index))}
                             style={{ width: cellWidth, margin: gap, justifyContent: "center", overflow: "hidden" }}>
                             {
                                 renderItem({ item, index })
                             }
-                        </Animated.View>
+                        </View>
                     )
                 }
             </View>
