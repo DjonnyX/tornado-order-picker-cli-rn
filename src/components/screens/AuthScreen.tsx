f93270f52ef0ee42f649ca6a59711bf4d68cc181
djonnyx <djonnyx@gmail.com>
2021-07-05 20:01:35 +0300

    feat: improved getStores method

--- src/components/screens/AuthScreen.tsx
+++ src/components/screens/AuthScreen.tsx
@@ -5,7 +5,7 @@ import { Picker } from '@react-native-community/picker';
 import { View, TextInput, Text } from "react-native";
 import { connect } from "react-redux";
 import { CommonActions } from "@react-navigation/native";
-import { take, takeUntil } from "rxjs/operators";
+import { switchMap, take, takeUntil } from "rxjs/operators";
 import { IStore } from "@djonnyx/tornado-types";
 import { MainNavigationScreenTypes } from "../navigation";
 import { IAppState } from "../../store/state";
@@ -16,7 +16,7 @@ import { orderApiService, refApiService } from "../../services";
 import { SystemActions } from "../../store/actions/SystemAction";
 import { SimpleButton } from "../simple";
 import { IAlertState } from "../../interfaces";
-import { Subject } from "rxjs";
+import { interval, Subject } from "rxjs";
 
 interface IFormSNProps {
     themeName: string;
@@ -248,6 +248,26 @@ const AuthScreenContainer = React.memo(({ _theme, _serialNumber, _setupStep, _te
 
     useEffect(() => {
         const unsubscribe$ = new Subject<void>();
+
+        const getStoresInterval = () => {
+            interval(5000).pipe(
+                take(1),
+                takeUntil(unsubscribe$),
+                switchMap(_ => {
+                    return refApiService.getStores({
+                        serial: _serialNumber,
+                    }).pipe(
+                        take(1),
+                        takeUntil(unsubscribe$),
+                    )
+                })
+            ).subscribe(
+                v => {
+                    setStores(v);
+                    getStoresInterval();
+                }
+            );
+        };
 
         if (_setupStep === 1) {
             refApiService.getStores({
